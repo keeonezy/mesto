658eb7d65d60ae902056a509a3340620220b5a60
Alexander Malukhin <keeonenew@yandex.ru>
2023-02-14 20:48:25 +0300

    feat: load array card

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,18 @@
-let popup = document.querySelector(".popup");
-let buttonEdit = document.querySelector(".profile__button-edit");
-let buttonClose = document.querySelector(".popup__close");
-let popupForm = document.querySelector(".popup__group");
+const popup = document.querySelector(".popup");
+const popupEditProfile = document.querySelector(".popup-edit-profile");
+const buttonEdit = document.querySelector(".profile__button-edit");
+const buttonClose = document.querySelector(".popup__close");
+const popupForm = document.querySelector(".popup__group");
 
 // Работа с инпутами
-let nameInput = document.querySelector(".popup__input_type_name");
-let jobInput = document.querySelector(".popup__input_type_job");
-let profileName = document.querySelector(".profile__title");
-let profileJob = document.querySelector(".profile__subtitle");
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_job");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
 
 
 
-popup.addEventListener("click", handleOverlayClose);
+popupEditProfile.addEventListener("click", handleOverlayClose);
 buttonEdit.addEventListener("click", handleOpenedPopup);
 buttonClose.addEventListener("click", handleClosePopup);
 popupForm.addEventListener("submit", handleFormSubmit);
@@ -19,13 +20,13 @@ popupForm.addEventListener("submit", handleFormSubmit);
 
 
 function handleOpenedPopup() {
-    popup.classList.add("popup_opened");
+    popupEditProfile.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
 function handleClosePopup() {
-    popup.classList.remove("popup_opened");
+    popupEditProfile.classList.remove("popup_opened");
 }
 
 function handleFormSubmit(evt) {
@@ -35,14 +36,24 @@ function handleFormSubmit(evt) {
     handleClosePopup();
 }
 
-function handleOverlayClose(event1) {
-    if (event1.target === event1.currentTarget) {
+function handleOverlayClose(evt) {
+    if (evt.target === evt.currentTarget) {
         handleClosePopup();
     }
 }
 
 
 //5 Часть
+// Работа с попапом
+const popupAddCard = document.querySelector(".popup-add-card");
+const addButtonCard = document.querySelector(".profile__button-add");
+addButtonCard.addEventListener("click", handleOpenedPopupCard);
+
+function handleOpenedPopupCard() {
+    popupAddCard.classList.toggle("popup_opened");
+}
+
+//Работа с карточками
 const initialCards = [
     {
         name: "Москва",
@@ -71,4 +82,21 @@ const initialCards = [
 ];
 
 const template = document.getElementById("card-li");
-template.document.querySelector(".card__image")
+const templatePaste = document.querySelector(".elements__list");
+
+const getCardTemplate = (card) => {
+    const newCard = template.content.cloneNode(true);
+    const newItem = newCard.querySelector(".card__title");
+    newItem.textContent = card.name;
+    const addLink = newCard.querySelector(".card__image");
+    addLink.src = card.link;
+    return newCard;
+}
+
+const renderItem = (wrap, card) => {
+    wrap.append(getCardTemplate(card))
+}
+
+initialCards.forEach((card) => {
+    renderItem(templatePaste, card)
+})
