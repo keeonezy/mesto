b8c3f24d3e7c43900bbab2042ff6e7c2096ef86a
Alexander Malukhin <keeonenew@yandex.ru>
2023-03-22 13:18:53 +0300

    refactor: js

--- scripts/Card.js
+++ scripts/Card.js
@@ -14,34 +14,33 @@ class Card {
         return newCard;
     }
 
-    _handleLikeButton() {
-        this._buttonLike.classList.toggle("card__like_active");
+    _handlerLikeButton() {
+        this._likeButton.classList.toggle("card__like_active");
     }
 
-    _handleImageViewer() {
+    _handlerDeleteButton() {
+        this._cardElement.remove();
+    }
+
+    _handlerImageViewer() {
         this._popupImage.src = this._link;
         this._popupImage.alt = this._name;
         this._popupFigcaption.textContent = this._name;
         openPopup(this._imageViewerPopup);
     }
 
-    _handleDeleteButton() {
-        this._cardElement.remove();
-    }
-
     _setEventListeners() {
-
-        this._buttonLike.addEventListener("click", () => this._handleLikeButton());
-        this._deleteButton.addEventListener("click", () => this._handleDeleteButton());
-        this._imageViewer.addEventListener("click", () => this._handleImageViewer());
+        this._likeButton.addEventListener("click", () => this._handlerLikeButton());
+        this._deleteButton.addEventListener("click", () => this._handlerDeleteButton());
+        this._imageViewer.addEventListener("click", () => this._handlerImageViewer());
     }
 
     createCard() {
         this._cardElement = this._getTemplate();
-        this._captionElement = this._cardElement.querySelector(".card__title");
-        this._buttonLike = this._cardElement.querySelector(".card__like");
+        this._likeButton = this._cardElement.querySelector(".card__like");
         this._deleteButton = this._cardElement.querySelector(".card__trash");
         this._imageElement = this._cardElement.querySelector(".card__image");
+        this._titleCard = this._cardElement.querySelector(".card__title");
         this._imageViewer = this._imageElement;
         this._popupImage = popupShowCard.querySelector(".popup__image");
         this._popupFigcaption = popupShowCard.querySelector(".popup__figcaption");
@@ -49,7 +48,7 @@ class Card {
 
         this._imageElement.src = this._link;
         this._imageElement.alt = this._name;
-        this._captionElement.textContent = this._name;
+        this._titleCard.textContent = this._name;
 
         this._setEventListeners();
 
